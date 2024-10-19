04076afa87659de2927801f10c207b78b616ae09
Sanzhar Ermekov <sangeek@outlook.com>
2024-10-19 18:46:24 +0300

    Initial Commit

---
+++ DeliveryCalculator/DeliveryCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryCalculatorApp.swift
+//  DeliveryCalculator
+//
+//  Created by macOS on 19.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
