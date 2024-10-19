320dfa6cc88cfaf6c801c7fd8269dcf203b793bc
Sanzhar Ermekov <sangeek@outlook.com>
2024-10-20 01:36:05 +0300

    Basic functionality

---
+++ DeliveryCalculator/DistanceCell.swift
@@ -0,0 +1,18 @@
+//
+//  DistanceCell.swift
+//  DeliveryCalculator
+//
+//  Created by macOS on 20.10.2024.
+//
+
+import SwiftUI
+
+struct DistanceCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DistanceCell()
+}
---
+++ DeliveryCalculator/WeightCell.swift
@@ -0,0 +1,18 @@
+//
+//  WeightCell.swift
+//  DeliveryCalculator
+//
+//  Created by macOS on 20.10.2024.
+//
+
+import SwiftUI
+
+struct WeightCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeightCell()
+}
